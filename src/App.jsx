108db1e7ad01fb61ed20bcf7788f5389b6e27613
TaylorBrooks314 <Taylormarybrooks01@gmail.com>
2023-07-27 10:00:12 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState,useEffect } from 'react'
+import './App.css'
+
+function App() {
+
+  async function getdata(){
+    let url= 
+    apiKey=8257c75632784d01af59abaed081edd6
+    let baseSearch= `https://api.spoonacular.com/recipes/complexSearch?`
+    let id=
+    let baseInfo=`https://api.spoonacular.com/recipes/${id}/information?`
+    const response= await fetch()
+    const data= await response.json()
+  }
+  useEffect(() => {
+// getData()
+  },[])
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
