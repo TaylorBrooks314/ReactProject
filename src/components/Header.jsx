aa5e61e2e04ae60d5763fa8cfc61b776e23a0cfa
TaylorBrooks314 <Taylormarybrooks01@gmail.com>
2023-08-03 11:27:08 -0600

    fixing logo3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,10 +9,9 @@ export default function Header(){
     console.log("version 1")
     return(
         <div>
-           
             <button onClick={handleClick} className="top">Search again</button>
             <div className="Header">
-            <img src="src\assets\logo.jpeg" className="headerImg"/>
+            <img src="logo.jpeg" className="headerImg"/>
             </div>
         </div>
     )
